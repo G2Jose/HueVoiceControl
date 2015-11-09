3ad5ca19ae239d3a36797f9f57a0b54bb100eeca
gejose <george.jose.a@gmail.com>
2015-11-09 00:14:19 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name raspberrypivoicecontrolhueApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the raspberrypivoicecontrolhueApp
+ */
+angular.module('raspberrypivoicecontrolhueApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name raspberrypivoicecontrolhueApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the raspberrypivoicecontrolhueApp
+ */
+angular.module('raspberrypivoicecontrolhueApp')
+  .controller('MainCtrl', function ($scope, $http) {
+
+  	$scope.turnOn = function(){
+
+  		$scope.method = 'PUT';
+	    $scope.url = 'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state';
+	    $scope.turnoffjson = '{"on":true}';
+	    this.urls = [
+	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state', 
+	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/2/state', 
+	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/3/state'
+	    ];
+	    for (var i = 0; i < this.urls.length; i ++ ){
+		    $http.put(this.urls[i], $scope.turnoffjson)
+	            .success(function(data){
+
+	               console.log(data);
+	            })
+	            .error(function(data){
+	            	$scope.message = 'error';
+	               console.log('Error: ' + data);
+	            });
+        }
+  		
+  	};
+
+  	$scope.turnOff = function(){
+  		$scope.method = 'PUT';
+	    $scope.url = 'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state';
+	    $scope.turnoffjson = '{"on":false}';
+	    this.urls = [
+	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state', 
+	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/2/state', 
+	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/3/state'
+	    ];
+	    for (var i = 0; i < this.urls.length; i ++ ){
+		    $http.put(this.urls[i], $scope.turnoffjson)
+	            .success(function(data){
+
+	               console.log(data);
+	            })
+	            .error(function(data){
+	            	$scope.message = 'error';
+	               console.log('Error: ' + data);
+	            });
+        }
+  		
+  	};
+
+
+
+
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
