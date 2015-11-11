9dc926a6f9c2fe12f6595c912ccba6a610f412d3
gejose <george.jose.a@gmail.com>
2015-11-10 22:35:31 -0500

    Fixed build

--- Gruntfile.js
+++ Gruntfile.js
@@ -444,7 +444,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*', 
+            'styles/*.css'
           ]
         }, {
           expand: true,
