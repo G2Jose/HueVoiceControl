c73a8cc954d678f19f973f01ecf0eff94fb5db98
gejose <george.jose.a@gmail.com>
2015-11-14 13:31:55 -0500

    Changed hue IP

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,12 +13,12 @@ angular.module('raspberrypivoicecontrolhueApp')
   	$scope.turnOn = function(){
 
   		$scope.method = 'PUT';
-	    $scope.url = 'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state';
+	    $scope.url = 'http://192.168.0.21/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state';
 	    $scope.turnoffjson = '{"on":true}';
 	    this.urls = [
-	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state', 
-	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/2/state', 
-	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/3/state'
+	    	'http://192.168.0.21/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state', 
+	    	'http://192.168.0.21/api/14681cbe10e72d072faf0cd21eda252b/lights/2/state', 
+	    	'http://192.168.0.21/api/14681cbe10e72d072faf0cd21eda252b/lights/3/state'
 	    ];
 	    for (var i = 0; i < this.urls.length; i ++ ){
 		    $http.put(this.urls[i], $scope.turnoffjson)
@@ -36,12 +36,12 @@ angular.module('raspberrypivoicecontrolhueApp')
 
   	$scope.turnOff = function(){
   		$scope.method = 'PUT';
-	    $scope.url = 'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state';
+	    $scope.url = 'http://192.168.0.21/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state';
 	    $scope.turnoffjson = '{"on":false}';
 	    this.urls = [
-	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state', 
-	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/2/state', 
-	    	'http://192.168.0.10/api/14681cbe10e72d072faf0cd21eda252b/lights/3/state'
+	    	'http://192.168.0.21/api/14681cbe10e72d072faf0cd21eda252b/lights/1/state', 
+	    	'http://192.168.0.21/api/14681cbe10e72d072faf0cd21eda252b/lights/2/state', 
+	    	'http://192.168.0.21/api/14681cbe10e72d072faf0cd21eda252b/lights/3/state'
 	    ];
 	    for (var i = 0; i < this.urls.length; i ++ ){
 		    $http.put(this.urls[i], $scope.turnoffjson)
